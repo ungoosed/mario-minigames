7e473af2dc4a86e76ec188960d0abc4049e998fc
ungoosed <ungoosed@gmail.com>
2024-10-06 19:48:29 -0700

    reindeer chat server all fixed and with send and username features

--- server/api/websocket.js
+++ server/api/websocket.js
@@ -6,14 +6,14 @@ export default defineWebSocketHandler({
     peer.publish(room, "joined chat");
   },
   close(peer) {
-    // console.log("closed ws", peer);
+    console.log("closed ws");
   },
   error(peer, error) {
-    // console.log("error!", peer, error);
+    console.log("error!", peer, error);
   },
   message(peer, message) {
     // console.log("message on ws", peer, message);
+    // peer.publish(room, message.text());
     peer.publish(room, message.text());
-    console.log(message.text());
   },
 });
