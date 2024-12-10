a330807dbb9f28b5c6a367171552504e7f96ccb7
ungoosed <ungoosed@gmail.com>
2024-12-09 21:33:43 -0800

    card selection syncing is implemented, need to do round lifecycle next

--- game/scenes/minigames/PicturePoker.js
+++ game/scenes/minigames/PicturePoker.js
@@ -9,11 +9,21 @@ export default class PicturePoker extends Scene {
     this.gameState = useGameState("gameState");
     this.userData = useUserData("userData");
     this.$bus = useNuxtApp().$bus;
+    this.hands = [];
     this.assets = {
       images: {
         "casino-background": "assets/minigames/table/casino-background.png",
+        coin: "assets/minigames/table/coin.png",
       },
-      spritesheets: {},
+      spritesheets: {
+        "hand-type-text": {
+          url: "assets/minigames/table/hand-type-text.png",
+          config: {
+            frameWidth: 110,
+            frameHeight: 15,
+          },
+        },
+      },
     };
     this.cardTypes = [
       "back",
@@ -54,6 +64,7 @@ export default class PicturePoker extends Scene {
         }
       }
     }
+    console.log(this.assets);
   }
   create() {
     this.handleMenu();
@@ -65,9 +76,9 @@ export default class PicturePoker extends Scene {
       this.add
         .image(128, 96, "casino-background")
         .setOrigin(0.5, 0.5)
-
-        .setRotation(Math.PI);
-      this.add.image(0, 192, "casino-background").setOrigin(0, 0);
+        .setRotation(Math.PI)
+        .setDepth(-1);
+      this.add.image(0, 192, "casino-background").setOrigin(0, 0).setDepth(-1);
       this.p2coins = this.add
         .bitmapText(15, 192, "ds", "30")
         .setOrigin(0, 0)
@@ -79,28 +90,48 @@ export default class PicturePoker extends Scene {
       if (this.gameState.value.data.round > 0) {
         for (let i = 0; i < this.gameState.value.data.users.length; i++) {
           if (this.gameState.value.users[i].id == this.userData.value.id) {
-            this.drawCards(50, 310, this.gameState.value.data.users[i].hand);
+            if (!this.hands[i]) {
+              this.hands[i] = this.drawCards(
+                50,
+                310,
+                this.gameState.value.data.users[i].hand,
+              );
+            } else {
+              //update own hand
+            }
           } else {
-            this.drawCards(20, 30 + i * 50, [0, 0, 0, 0, 0]);
+            if (!this.hands[i]) {
+              console.log("draw");
+              this.hands[i] = this.drawCards(20, 30 + i * 50, [0, 0, 0, 0, 0]);
+            } else {
+              //update other person hand
+            }
+          }
+          if (this.gameState.value.data.users[i].selected && this.hands[i]) {
+            this.hands[i].setSelected(
+              this.gameState.value.data.users[i].selected,
+            );
           }
         }
       }
     }.bind(this);
     let onTry = function (args) {
+      console.log(args);
+      // only update when it is their turn
       if (args.id == this.gameState.value.data.turn) {
+        let userIndex = indexOfUser(args.id);
         if (args.data.type == "select") {
-          let userIndex = this.gameState.value.data.users.findIndex((usr) => {
-            usr.id == args.id;
-          });
+          console.log("sending");
+          console.log(userIndex);
+
           this.gameState.value.data.users[userIndex].selected[args.data.card] =
             true;
+          this.broadcastStrippedGameState();
         }
         if (args.data.type == "deselect") {
-          let userIndex = this.gameState.value.data.users.findIndex((usr) => {
-            usr.id == args.id;
-          });
           this.gameState.value.data.users[userIndex].selected[args.data.card] =
             false;
+          this.broadcastStrippedGameState();
         }
       }
     }.bind(this);
@@ -111,9 +142,9 @@ export default class PicturePoker extends Scene {
     let cardObjects = [];
     for (let i = 0; i < cards.length; i++) {
       let interactiveCard = this.add.sprite(
-        x + i * 40,
-        y,
-        Object.keys(this.assets.spritesheets)[cards[i]],
+        128,
+        192,
+        this.cardTypes[cards[i]] + "-card",
       );
       if (!(cards[i] == 0)) {
         // animations
@@ -123,6 +154,13 @@ export default class PicturePoker extends Scene {
           .setInteractive()
           .setData("type", this.cardTypes[cards[i]])
           .setData("selected", false);
+        this.tweens.add({
+          targets: interactiveCard, // The sprite to move
+          x: x + i * 40, // The destination x-coordinate
+          y: y, // The destination y-coordinate
+          ease: "Linear", // Easing function
+          duration: 200, // Duration in milliseconds
+        });
         interactiveCard.on("pointerdown", () => {
           if (interactiveCard.getData("selected")) {
             interactiveCard.setData("selected", false);
@@ -133,7 +171,7 @@ export default class PicturePoker extends Scene {
               ease: "Power1", // Easing function
               duration: 200, // Duration in milliseconds
             });
-            this.$bus.emit("action", { type: "select", card: i });
+            this.$bus.emit("action", { type: "deselect", card: i });
           } else {
             interactiveCard.setData("selected", true);
             this.tweens.add({
@@ -143,7 +181,7 @@ export default class PicturePoker extends Scene {
               ease: "Power1", // Easing function
               duration: 200, // Duration in milliseconds
             });
-            this.$bus.emit("action", { type: "deselect", card: i });
+            this.$bus.emit("action", { type: "select", card: i });
           }
         });
       } else {
@@ -151,7 +189,21 @@ export default class PicturePoker extends Scene {
       }
       cardObjects.push(interactiveCard);
     }
-    return cardObjects;
+    return {
+      objects: cardObjects,
+      setSelected: (hand) => {
+        for (let i = 0; i < hand.length; i++) {
+          cardObjects[i].setData("selected", hand[i]);
+          this.tweens.add({
+            targets: cardObjects[i], // The sprite to move
+            x: x + i * 40, // The destination x-coordinate
+            y: y - (hand[i] ? 20 : 0), // The destination y-coordinate
+            ease: "Power1", // Easing function
+            duration: 200, // Duration in milliseconds
+          });
+        }
+      },
+    };
   }
   createGlobalAnims() {
     this.anims.create({
@@ -303,43 +355,38 @@ export default class PicturePoker extends Scene {
       let hand = generateHand();
       this.gameState.value.data.users[i].hand = hand;
       // if client is not host
-      console.log(toRaw(this.gameState.value.users));
-      if (
-        !(this.gameState.value.users[i].id == this.gameState.value.users[0].id)
-      ) {
-        this.$bus.emit("targetupdate", {
-          data: this.strippedGameState(this.gameState.value.users[i].id),
-          target: this.gameState.value.users[i].id,
-        });
+    }
+    this.broadcastStrippedGameState();
+  }
+  strippedGameState(user) {
+    let i = indexOfUser(user);
+    let strippedCopy = structuredClone(toRaw(this.gameState.value.data));
+    for (let k = 0; k < this.gameState.value.users.length; k++) {
+      if (k == i) {
+        strippedCopy.users[i].hand = this.gameState.value.data.users[i].hand;
+      } else {
+        strippedCopy.users[i].hand = [0, 0, 0, 0, 0];
       }
     }
+    return strippedCopy;
+  }
+  broadcastStrippedGameState() {
+    for (let i = 1; i < this.gameState.value.users.length; i++) {
+      // send targeted update to everyone but the host
+      let strippedState = this.strippedGameState(
+        this.gameState.value.users[i].id,
+      );
+      console.log(strippedState);
+      console.log(this.gameState.value.users[i].id);
+      this.$bus.emit("targetupdate", {
+        data: strippedState,
+        target: this.gameState.value.users[i].id,
+      });
+    }
+    //send full game data to self (host) also to trigger stuff, prob not as efficient as it could be but whatever
     this.$bus.emit("targetupdate", {
       data: this.gameState.value.data,
       target: this.userData.value.id,
     });
-  }
-  strippedGameState(user, hand) {
-    let i = indexOfUser(user);
-    console.log(user);
-    console.log(i);
-    let strippedCards = [];
-    for (let k = 0; k < this.gameState.value.users.length; k++) {
-      if (k == i) {
-        strippedCards.push({
-          id: this.gameState.value.users[i].id,
-          coins: 0,
-          hand: this.gameState.value.data.users[i].hand,
-        });
-      } else {
-        strippedCards.push({
-          id: this.gameState.value.users[i].id,
-          coins: 0,
-          hand: [0, 0, 0, 0, 0],
-        });
-      }
-    }
-    let strippedCopy = structuredClone(toRaw(this.gameState.value.data));
-    strippedCopy.users = strippedCards;
-    return strippedCopy;
   }
 }
