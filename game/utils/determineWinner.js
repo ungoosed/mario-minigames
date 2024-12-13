c0cf4743981f316065a6b579e610cbb5648afabf
ungoosed <ungoosed@gmail.com>
2024-12-13 11:40:32 -0800

    made determineWinner function, still need to see if it works

--- game/utils/determineWinner.js
+++ game/utils/determineWinner.js
@@ -3,12 +3,12 @@ export default function determineWinner(handsArr) {
   let winner = 0;
   for (let i = 0; i < handsArr.length; i++) {
     let cardCount = [0, 0, 0, 0, 0, 0];
-    let sortedCards = [0, 0, 0, 0, 0];
     let junk = [];
     let highest = {
       type: 0,
       suit: 0,
       suit2: 0,
+      sorted: [0, 0, 0, 0, 0],
     };
     for (let card in handsArr[i]) {
       cardCount[card]++;
@@ -19,48 +19,72 @@ export default function determineWinner(handsArr) {
       } else if (cardCount[i] == 2) {
         highest.type++;
         if (highest.type == 3) {
-          sortedCards.fill(i, 3, 5);
+          highest.sorted.fill(i, 3, 5);
           highest.suit2 = i;
         } else if (highest.type == 1) {
-          highest.suit2 = i;
-          sortedCards.fill(i, 2, 4);
+          if (highest.suit < i) {
+            highest.suit2 = highest.suit;
+            highest.suit = i;
+          }
+          highest.sorted.fill(i, 2, 4);
         } else {
           highest.suit = i;
-          sortedCards.fill(i, 0, 2);
+          highest.sorted.fill(i, 0, 2);
         }
       } else if (cardCount[i] == 3) {
         if (highest.type == 1) {
           highest.type = 4;
-          sortedCards[3] = sortedCards[0];
-          sortedCards[4] = sortedCards[1];
+          highest.sorted[3] = highest.sorted[0];
+          highest.sorted[4] = highest.sorted[1];
           highest.suit2 = highest.suit;
         } else {
           highest.type = 3;
         }
         highest.suit = i;
-        sortedCards.fill(i, 0, 3);
+        highest.sorted.fill(i, 0, 3);
       } else if (cardCount[i] == 4) {
         highest.type = 5;
         highest.suit = i;
-        sortedCards.fill(i, 0, 4);
+        highest.sorted.fill(i, 0, 4);
       } else if (cardCount[i] == 5) {
         highest.type = 6;
         highest.suit = i;
-        sortedCards.fill(i, 0, 5);
+        highest.sorted.fill(i, 0, 5);
       }
     }
     for (let i = 0; i < junk.length; i++) {
-      sortedCards[sortedCards.length - i - 1] = junk[i];
+      highest.sorted[highest.sorted.length - i - 1] = junk[i];
     }
-    finalCards.push({
-      type: highest,
-      cards: sortedCards,
-    });
+    finalCards.push(highest);
   }
-  let highest = {
+  let overallHighest = {
     type: 0,
     suit: 0,
     suit2: 0,
+    winners: [],
+    cards: finalCards,
   };
-  for (let i = 0; i < finalCards.length; i++) {}
+  for (let i = 0; i < finalCards.length; i++) {
+    if (finalCards[i].type > overallHighest.type) {
+      overallHighest.type = finalCards[i].type;
+      overallHighest.suit = finalCards[i].suit;
+      overallHighest.suit2 = finalCards[i].suit2;
+      overallHighest.winners = [i];
+    }
+    if (finalCards[i].type == overallHighest.type) {
+      if (finalCards[i].suit > overallHighest.suit) {
+        overallHighest.suit = finalCards[i].suit;
+        overallHighest.suit2 = finalCards[i].suit2;
+        overallHighest.winners.push(i);
+      } else if (finalCards[i].suit == overallHighest.suit) {
+        if (finalCards[i].suit2 > overallHighest.suit2) {
+          overallHighest.suit2 = finalCards[i].suit2;
+          overallHighest.winners = [i];
+        } else {
+          overallHighest.winners.push(i);
+        }
+      }
+    }
+  }
+  return overallHighest;
 }
